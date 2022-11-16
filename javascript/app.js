ba6edbeefe6c5037b02a6df2450e9e353c94fbb2
unknown <minnmin1344@gmail.com>
2022-11-16 08:53:57 +0700

    practice

---
+++ javascript/app.js
@@ -0,0 +1,20 @@
+// alert('hello world')
+// let a = true
+// console.log(a)
+
+let distance = -1;
+if (distance <= 0) {
+    console.log("Invalid value");
+}
+else if(distance>10){
+    console.log("di xe oto")
+}
+else if(distance>5) {
+    console.log("di xe may")
+}
+else if(distance>1){
+    console.log("di xe dap")
+}
+else{
+    console.log("di bo")
+}
