00860d611bfca845c51dfbb3cd409e07722db500
unknown <minnmin1344@gmail.com>
2022-11-17 07:11:39 +0700

    practice2

---
+++ javascript homework/app.js
@@ -0,0 +1,57 @@
+let X = 4.5;
+
+if(X==0) {
+    console.log("x=0")
+}
+else if(X>0) {
+    if(Number.isInteger(X)) console.log("so nguyen duong");
+    else console.log("so thap phan duong")
+}
+else{
+    console.log("so am")
+}
+
+// function max(a, b, c){
+//     let _max = a;
+//     if (_max < b) _max = b;
+//     if (_max < c) _max = c;
+//     return _max
+// }
+
+// let a = 1
+// let b = 5
+// let c = 4 
+// console.log(max(a, b, c))
+
+// function max(a, b, c){
+    // let _max = a;
+    // if (_max < b) _max = b;
+    // if (_max < c) _max = c;
+    // return _max
+// }
+
+// let a = 4
+// let b = 6
+// let c = 8
+// console.log(max(a, b, c))
+
+// let diemTrungBinh = 8 ;
+// if(diemTrungBinh>=9.0){
+    // console.log("la hang A")
+// }
+
+// else if(diemTrungBinh>=7.0){
+    // console.log("la hang B")
+// }
+
+// else if(diemtrungbinh>=5.0){
+    // console.log("la hang C")
+// }
+
+// else{
+    // console.log("la hang F")
+// }
+
+
+
+
